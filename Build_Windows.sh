c2b7aceff651748472f72714894ec5903e1b1ccd
JackoboLeChocobo <imbert.jacquesolivier@gmail.com>
2024-11-01 11:38:27 +0100

    Update Build_Windows.sh

--- Build_Windows.sh
+++ Build_Windows.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # sudo apt-get install mingw-w64 g++-mingw-w64
 
+ls /usr/bin/i686*
 i686-w64-mingw32-gcc-12-win32 main.c -I./windows_deps/include/SDL2 -I./windows_deps/include/libusb-1.0 -L./windows_deps/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lusb-1.0 -mwindows -o MD_Dumper.exe
 mkdir Windows_Build
 cp -dR Informations.txt ./Windows_Build/
@@ -9,8 +10,3 @@ cp MD_Dumper.exe ./Windows_Build/
 cp -dR images ./Windows_Build/
 cp -dR MD_Dumper_GUI.bat ./Windows_Build/
 cp ./windows_deps/files/bin/*.dll ./Windows_Build
-rm -rf CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-
